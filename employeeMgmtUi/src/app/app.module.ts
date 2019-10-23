6da15c404d6c8088b7a5420415b42262c2e3993d
Rupesh <rupesh.sripuram@gmail.com>
2019-10-23 14:38:47 +0530

    new structure

--- employeeMgmtUi/src/app/app.module.ts
+++ employeeMgmtUi/src/app/app.module.ts
@@ -1,34 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
-import {CommonService} from './service/common.service';
 
-import { LoginService } from './login/login.service';
-import { ApiService } from './service/api.service';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import {FormsModule , ReactiveFormsModule  } from '@angular/forms';
-import { FieldErrorDisplayComponent } from './field-error-display/field-error-display.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    FieldErrorDisplayComponent,
-    DashboardComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    AppRoutingModule
   ],
-  providers: [CommonService,LoginService,ApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
