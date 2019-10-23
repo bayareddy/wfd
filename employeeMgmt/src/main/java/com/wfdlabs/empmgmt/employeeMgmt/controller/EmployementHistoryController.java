e16ada52aba6ce74b9c6a3eec225504485fa9244
GongadiAshok <gongadi.ashok@gmail.com>
2019-10-23 15:21:08 +0530

    @crossorigin Annotation updated

--- employeeMgmt/src/main/java/com/wfdlabs/empmgmt/employeeMgmt/controller/EmployementHistoryController.java
+++ employeeMgmt/src/main/java/com/wfdlabs/empmgmt/employeeMgmt/controller/EmployementHistoryController.java
@@ -6,6 +6,7 @@ import java.util.NoSuchElementException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,6 +22,7 @@ public class EmployementHistoryController {
 	@Autowired
 	private EmployementHistoryService employementHistoryService;
 
+	@CrossOrigin(origins="*")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<EmployementHistory> createEmployeeHistory(
 			@RequestBody EmployementHistory employementHistory) {
@@ -28,7 +30,7 @@ public class EmployementHistoryController {
 		employementHistory = employementHistoryService.createEmployementHistory(employementHistory);
 		return new ResponseEntity<>(employementHistory, HttpStatus.CREATED);
 	}
-
+	@CrossOrigin(origins="*")
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<EmployementHistory> getEmployementHistory(@RequestParam Integer employeeId) {
 		EmployementHistory employementHistory = null;
@@ -43,7 +45,7 @@ public class EmployementHistoryController {
 		}
 		return new ResponseEntity<>(employementHistory, HttpStatus.OK);
 	}
-
+	@CrossOrigin(origins="*")
 	@RequestMapping(value = "/getall", method = RequestMethod.GET)
 	public ResponseEntity<List<EmployementHistory>> getAllEmployementHistory() {
 		List<EmployementHistory> employementHistoryList = employementHistoryService.getAllEmployementHistory();
@@ -52,14 +54,14 @@ public class EmployementHistoryController {
 		}
 		return new ResponseEntity<>(employementHistoryList, HttpStatus.OK);
 	}
-
+	@CrossOrigin(origins="*")
 	@RequestMapping(method = RequestMethod.DELETE)
 	public ResponseEntity<Object> deleteEmployementHistory(@RequestParam Integer employeeId) {
 		System.out.println("employeeId:" + employeeId);
 		employementHistoryService.deleteEmployementHistory(employeeId);
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
-
+	@CrossOrigin(origins="*")
 	@RequestMapping(method = RequestMethod.PUT)
 	public ResponseEntity<EmployementHistory> updateEmployementHistory(
 			@RequestBody EmployementHistory employementHistory) {
