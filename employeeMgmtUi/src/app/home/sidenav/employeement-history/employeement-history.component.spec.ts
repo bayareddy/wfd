625a70bcbde54ca035d64b7509f77dfaa2cac3d0
vamshi901432 <lokasanivamshidarreddy@gmail.com>
2019-12-20 08:15:49 +0530

    src/app/home/sidenav/employeement-history

---
+++ employeeMgmtUi/src/app/home/sidenav/employeement-history/employeement-history.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EmployeementHistoryComponent } from './employeement-history.component';
+
+describe('EmployeementHistoryComponent', () => {
+  let component: EmployeementHistoryComponent;
+  let fixture: ComponentFixture<EmployeementHistoryComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ EmployeementHistoryComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EmployeementHistoryComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ employeeMgmtUi/src/app/home/sidenav/employeement-history/employeement-history.component.ts
@@ -0,0 +1,116 @@
+import { Component, OnInit } from '@angular/core';
+import {NgxPaginationModule} from 'ngx-pagination';
+import { RestService } from './service/rest.service';
+
+@Component({
+  selector: 'app-employeement-history',
+  templateUrl: './employeement-history.component.html',
+  styleUrls: ['./employeement-history.component.css']
+})
+export class EmployeementHistoryComponent implements OnInit {
+  public myData:any = {};
+ 
+  
+  public BASE_URL:string = 'http://ec2-3-6-45-30.ap-south-1.compute.amazonaws.com:8081/';
+  
+  constructor(private service :RestService) { }
+
+  ngOnInit() {
+    this.getData();
+    
+   
+  }
+addData(data){
+    console.log("4444444444444444444444", data); 
+
+    let url = `${this.BASE_URL}/EmployementHistory`;
+
+    
+
+    this.service.post(url,data).subscribe(backenddata => {
+      console.log("11111111111111", backenddata);
+
+
+      this.ngOnInit();
+      
+
+      
+    });
+
+  }
+
+
+
+  
+
+
+
+public regData:any = [];
+
+    getData()
+    {
+      let url = `${this.BASE_URL}/EmployementHistory/getall`;
+      this.service.get(url).subscribe(backdata => {
+
+          this.regData = backdata as string[];
+
+          
+
+      });
+    }
+
+
+ DeleteData(i)
+
+{
+  
+ 
+
+  let url = `${this.BASE_URL}/EmployementHistory?employeeId=`;
+
+url=url+i.employementHistoryId;
+
+
+
+  this.service.delete(url).subscribe(backdata => {
+
+          this.regData = backdata as string[];
+
+          console.log(this.regData);
+this.ngOnInit();
+      });
+
+
+}
+
+
+
+
+
+ReflectData(item)
+{
+
+console.log(item);
+
+this.myData=item;
+
+console.log(this.myData);
+
+
+
+
+
+
+
+}
+
+  
+  
+
+}
+
+
+
+
+  
+
