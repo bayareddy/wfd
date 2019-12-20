2bbba5fbfedcc891a461dc92312e828e1332b87e
vamshi901432 <56412412+vamshi901432@users.noreply.github.com>
2019-12-20 08:22:42 +0530

    Add files via upload

---
+++ employeeMgmtUi/src/app/app-routing.module.ts
@@ -0,0 +1,60 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeadernavComponent } from './home/headernav/headernav.component';
+import { FooternavComponent } from './home/footernav/footernav.component';
+import { SidenavComponent } from './home/sidenav/sidenav.component';
+import { RoleComponent } from './home/sidenav/role/role.component';
+import { LeaveStatusComponent } from './home/sidenav/leave-status/leave-status.component';
+import { MainContentComponent } from './home/main-content/main-content.component';
+import {AcademicsComponent} from'./home/sidenav/academics/academics.component';
+import { LeaveTypeComponent } from './home/sidenav/leave-type/leave-type.component';
+import { ForgetPasswordComponent } from './login/forget-password/forget-password.component';
+import { EmployeementHistoryComponent } from './home/sidenav/employeement-history/employeement-history.component';
+
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    
+    children: [
+      {
+        path: 'home', component: HomeComponent,
+        children: [
+          { path: 'sideNavRole', component: RoleComponent },
+		   { path: 'emphis', component: EmployeementHistoryComponent },
+          { path: 'sideNavLeaveStatus', component: LeaveStatusComponent },
+          {path:'sideNavAcademics', component:AcademicsComponent},
+          { path: 'leaveType', component:LeaveTypeComponent},
+          { path: '', component: MainContentComponent },
+        ]
+      },
+
+     
+      { path: "", component: LoginComponent }
+
+    ],
+    component: DashboardComponent
+  },
+  
+  { path: 'login', component: LoginComponent },
+  {path:'forgetpassword', component:ForgetPasswordComponent},
+  { path: '**', redirectTo: '/login' },
+  { path: '**', redirectTo: '/login', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(appRoutes, {
+      useHash: true
+    })
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule { }
