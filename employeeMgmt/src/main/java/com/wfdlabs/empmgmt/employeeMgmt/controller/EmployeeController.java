d474a9df39c37583d43c4e013a5b8d7029d14e82
syam143 <syam1231942gmail.com>
2019-12-26 21:12:45 +0530

    generate pdf

--- employeeMgmt/src/main/java/com/wfdlabs/empmgmt/employeeMgmt/controller/EmployeeController.java
+++ employeeMgmt/src/main/java/com/wfdlabs/empmgmt/employeeMgmt/controller/EmployeeController.java
@@ -1,12 +1,18 @@
 package com.wfdlabs.empmgmt.employeeMgmt.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import javax.persistence.NoResultException;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,6 +26,7 @@ import com.wfdlabs.empmgmt.employeeMgmt.service.EmployeeService;
 @RequestMapping("/employee")
 @RestController
 public class EmployeeController {
+
 	@Autowired
 	EmployeeService employeeService;
 
@@ -82,6 +89,32 @@ public class EmployeeController {
 	public String deleteUser(@PathVariable Integer employeeId) {
 		return employeeService.deleteEmployee(employeeId);
 
+	}
+	
+
+	//@SuppressWarnings("resource")
+	@RequestMapping(value = "/getPdf", method = RequestMethod.GET, produces = "application/pdf")
+	public ResponseEntity<byte[]> getPdf() {
+		FileInputStream fileStream;
+		File file = new File("D:\\Syam.pdf");
+
+		try {
+			fileStream = new FileInputStream(file);
+			byte contents[] = new byte[(int) file.length()];
+			fileStream.read(contents);
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.parseMediaType("application/pdf"));
+			String filename = "Syam.pdf";
+			headers.setContentDispositionFormData(filename, filename);
+			ResponseEntity<byte[]> response = new ResponseEntity<byte[]>(contents, headers, HttpStatus.OK);
+			return response;
+		} catch (FileNotFoundException e) {
+			System.err.println(e);
+		} catch (IOException e) {
+			System.err.println(e);
+		}
+		return null;
 	}
 
 }
