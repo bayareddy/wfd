d9cbffce215aed02772a4c511799aeb683b68741
PardhaSaradhi Personal <mannempardhu@gmail.com>
2020-02-05 11:27:28 +0530

    Bank deatails tab added nad edit and submit buttons added

--- employeeMgmtUi/src/app/home/sidenav/my-profile/my-profile.component.ts
+++ employeeMgmtUi/src/app/home/sidenav/my-profile/my-profile.component.ts
@@ -14,24 +14,29 @@ export class MyProfileComponent implements OnInit {
   ) { }
   currentUsers = {}
   currentUser = {}
-  paidSalary={}
-  bankDetails={}
+  paidSalary = {}
+  bankDetails = {}
   eid = '';
+  hidden = false;
+  visible = false;
+  alert=false;
+  bankError=false;
 
   ngOnInit() {
 
     this.currentUsers = this.loginService.currentUser;
-    console.log('user data is ',this.currentUsers);
+    console.log('user data is ', this.currentUsers);
     console.log('current user Employee ID' + this.currentUsers['employeeId']);
     this.getById();
     this.eid = this.currentUsers['employeeId'];
-    this.paidSalary=this.currentUsers['paidSalary'][0]
- this.bankDetails=this.currentUsers['bankDetails']
+    this.paidSalary = this.currentUsers['paidSalary'][0]
+    //this.bankDetails=this.currentUsers['bankDetails']
 
-    console.log('paid salary is',this.paidSalary['id'])
-    console.log('bank Details are',this.bankDetails['id'])
+    console.log('paid salary is', this.paidSalary['id'])
+    console.log('bank Details are', this.bankDetails['id'])
     console.log(this.eid);
     console.log(this.currentUsers['employeeId'])
+    this.getBankById()
   }
 
 
@@ -43,6 +48,45 @@ export class MyProfileComponent implements OnInit {
       this.currentUser = this.currentUsers;
     }
     )
+  }
+
+
+  editBtn() {
+    this.hidden = true;
+
+
+  }
+
+  getBankById() {
+    console.log('id is' + this.currentUsers['employeeId'])
+    this.profile.getBankByIdService(this.currentUsers['employeeId'])
+      .subscribe((res) => {
+        console.log(res)
+        this.bankDetails = res;
+
+      })
+  }
+
+
+  submitApi(bankDetails) {
+    let url = `${this.baseUrl}/bankdetails`;
+    console.log('bank details are ', bankDetails)
+    this.profile.editBank(url, bankDetails)
+
+      .subscribe(responce => {
+        console.log(responce)
+        this.alert=true;
+        setTimeout(() => {
+          this.alert=false;
+        }, 3000);
+
+      }, (error) => {
+        this.bankError=true;
+        setTimeout(() => {
+          this.bankError=false;
+        }, 3000);
+      })
+    this.hidden = false;
   }
 
 }
--- employeeMgmtUi/src/app/service/myprofile.service.ts
+++ employeeMgmtUi/src/app/service/myprofile.service.ts
@@ -6,11 +6,23 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root'
 })
 export class MyprofileService {
+  
+  baseUrl = 'http://employemanagment-env.6dpm2iqwkd.ap-south-1.elasticbeanstalk.com:8080';
+
 
   constructor(private http:HttpClient) { }
 
 getProfile(url){
 
 return this.http.get(url)
+}
+
+editBank(url,data){
+return this.http.put(url,data);
+}
+
+getBankByIdService(id){
+  let url=`${this.baseUrl}/bankdetails/${id}`
+ return this.http.get(url)
 }
 }
